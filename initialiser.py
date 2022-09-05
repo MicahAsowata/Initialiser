664592cab5d31b5ae84119887b8076c79dc5afa6
Micah Asowata <93443076+MicahAsowata@users.noreply.github.com>
2022-09-05 11:56:04 +0100

    Updated names according to conventions

--- initialiser.py
+++ initialiser.py
@@ -1,5 +1,5 @@
-def initialiser(name: str, shouldBeUpperCase: bool = False) -> str:
-    if shouldBeUpperCase:
+def initialiser(name: str, should_be_upper_case: bool = False) -> str:
+    if should_be_upper_case:
         return name[0 : 1].upper()
     else:
         return name[0 : 1]
